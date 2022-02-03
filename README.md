4d007b83ea192206aa947b0aff41525d5257d15e
jan Sono <doris1964@protonmail.com>
2022-02-03 15:46:42 -0500

    Reinit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Graphical Dictionary of Toki Pona
+
+This dictionary is organized to assist in the reading of
+Toki Pona in the *sitelen pona*
+system of hieroglyphs.  The glyphs are grouped
+according to their general shape to make it easier to
+see differences between similar signs.  The definitions
+follow.
+
+Shield: [![CC BY 4.0][cc-by-shield]][cc-by]
+
+This work is licensed under a
+[Creative Commons Attribution 4.0 International License][cc-by].
+
+[![CC BY 4.0][cc-by-image]][cc-by]
+
+[cc-by]: http://creativecommons.org/licenses/by/4.0/
+[cc-by-image]: https://i.creativecommons.org/l/by/4.0/88x31.png
+[cc-by-shield]: https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg
---
+++ glyphs.lua
@@ -0,0 +1,73 @@
+-- Code to lay out toki pona glyphs in arrays by shape class.
+
+-- Start a group  The name goes out in bold, followed by
+-- the start of the table with the proper number of
+-- columns.
+function grouphead(name)
+  tex.print(string.format("\\section*{%s}", name))
+  tex.print("\\begin{tabular}{")
+  for col=1, wid do
+    tex.sprint(" p{1.2cm}")
+    end
+  tex.sprint(" }")
+  end
+
+-- Close out the table.
+function endgroup()
+  tex.print("\\end{tabular}")
+  end
+
+-- Process a group of similarly shaped glyphs.  The first word
+-- in the string is the group name.
+function group(line)
+  local wn = 0		       -- Word Number
+  local col = 1		       -- Column Number
+  for word in (line):gmatch "%a*" do
+    if(wn == 0)		    -- First word is the group name
+    then
+      grouphead(word)		-- Emit header and start table
+      col = 1
+    else
+      if( col > wid )		-- Exceeded max columns
+      then
+        tex.sprint(" \\\\[0.5cm]")
+	col = 1
+      else
+        if( col > 1 )		-- Additional column
+	then
+	  tex.sprint(" & ")
+	end
+      end
+      tex.sprint(string.format("\\tpGlyph{%s}", word)) -- Emit one glyph cell
+      col = col + 1
+      end
+    wn = wn + 1
+    end
+  if( col > 1 )		-- End of string. Partial row already out?
+  then
+    tex.sprint(" \\\\")
+  end
+  endgroup()		-- Finish the table
+  end
+
+wid = 10			-- Maximum columns
+
+-- Output all the words
+group("Circles ijo jan lawa kili ma mama mani meli\
+mije tonsi moli nasa sike suno tenpo toki lape nimi\
+palisa misikeke lukin")
+group("D alasa kalama uta")
+group("Six mi ona sina")
+group("Squares lipu len sona sitelen pu pakala\
+monsi sinpin anpa insa poka open poki")
+group("Straight a lon nasin epiku ni o pini taso nanpa wan\
+sama tu mute meso sin seli supa selo sijelo")
+group("Angles ala ante weka anu e li suli lili utala\
+monsuta pan en lete ken pi")
+group("Triangles kule laso loje walo jelo pimeja")
+group("Curves ale ike pona la kon telo jasima kute linja musi nena seme tan wile")
+group("Hand luka kepeken pana pali moku")
+group("Feet noka awen tawa kama")
+group("Animals akesi kala pipi soweli suwi mu")
+group("Squiggles unpa lanpan jaki esun sewi jo kasi")
+group("Other olin pilin mun ko kiwen tomo ilo kulupu oko")
